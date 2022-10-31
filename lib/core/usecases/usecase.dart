d3636a0ffdd67a0dc9fb79f73d82389fdbd950ef
Sep <s.heydari0@gmail.com>
2022-10-31 12:31:07 +0330

    updated params class with apiKey

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,13 +1,14 @@
 import 'package:equatable/equatable.dart';
 
+const String apiKey = '31746fb4b791cab85b915be7c308614f';
+
 class Params extends Equatable {
+  static const String appid = apiKey;
   final double lon;
   final double lat;
-  final String appid;
-  final String? units;
+  final String units;
 
-  const Params(
-      {required this.lon, required this.lat, required this.appid, this.units});
+  const Params({required this.lon, required this.lat, this.units = 'metric'});
   @override
   List<Object?> get props => [
         lon,
