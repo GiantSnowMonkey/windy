545fbd90ed9629c476d69489710c90d0094ce620
Sep <s.heydari0@gmail.com>
2022-10-31 12:47:31 +0330

    Added RemoteDataSrc and RemoteDataSrcImpl

---
+++ lib/weather/data/datasources/weather_info_remote_datasource.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+
+import '../../../../core/errors/exceptions.dart';
+import '../../../core/usecases/usecase.dart';
+import '../models/weather_info_model.dart';
+
+const String siteUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+abstract class WeatherInfoRemoteDataSource {
+  Future<WeatherInfoModel>? getWeatherInfo(Params params);
+}
+
+class WeatherInfoRemoteDataSourceImpl implements WeatherInfoRemoteDataSource {
+  final Dio dio;
+
+  WeatherInfoRemoteDataSourceImpl({required this.dio});
+
+  @override
+  Future<WeatherInfoModel>? getWeatherInfo(Params params) async {
+    final response = await dio.get(
+      siteUrl,
+      queryParameters: {
+        'lat': params.lat,
+        'lon': params.lon,
+        'appid': Params.appid,
+        'metric': params.units
+      },
+    );
+    if (response.statusCode == 200) {
+      return WeatherInfoModel.fromJson(response.data);
+    } else {
+      throw ServerException();
+    }
+  }
+}
